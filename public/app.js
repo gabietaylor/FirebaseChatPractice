24b669830be22cb41acd4888108d157d4f7b0158
gabietaylor <gabie.taylor@yahoo.com>
2017-08-27 12:56:49 -0400

    firebase config added

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA4V8sBwg8pK9UOzedHnfkHDy5qivWLpkE",
+    authDomain: "fbchatprac.firebaseapp.com",
+    databaseURL: "https://fbchatprac.firebaseio.com",
+    projectId: "fbchatprac",
+    storageBucket: "fbchatprac.appspot.com",
+    messagingSenderId: "752608176062"
+};
+firebase.initializeApp(config);
